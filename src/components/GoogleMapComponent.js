5e3b0945c44830c5170d0aa6f133856b7bafc067
タマキジュンヤ <tmkjny@gmail.com>
2023-10-05 16:16:49 +0900

    2023/10/05

---
+++ src/components/GoogleMapComponent.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const containerStyle = {
+  width: '400px',
+  height: '400px'
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523
+};
+
+const GoogleMapComponent = () => {
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+        {/* 任意の子コンポーネントやマーカーをここに追加できます */}
+      </GoogleMap>
+    </LoadScript>
+  );
+}
+
+export default GoogleMapComponent;
